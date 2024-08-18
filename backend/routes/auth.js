998571ed98943e3f4d8865380b5b3a9063a2f635
Allan Araya Reyes <allanreyes0107@gmail.com>
2024-08-17 23:13:16 -0600

    Auth and routes

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,12 @@
 import express from "express";
-import { signup, login, logout } from "../controllers/auth.controller.js";
+import { getMe, signup, login, logout } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
+router.get("/me", protectRoute, getMe)
 router.post("/signup", signup);
-
-router.get("/login", login);
-
-router.get("/logout", logout);
+router.post("/login", login);
+router.post("/logout", logout);
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,18 @@ import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.js";
 import connectMongoDB from "./db/connectMongoDB.js"
+import cookieParser from "cookie-parser";
 
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); //parse req.body
+app.use(express.urlencoded({ extended: true})) //parse form data(urlencoded)
+app.use(cookieParser());
+
 
 app.use("/api/auth", authRoutes);
 
