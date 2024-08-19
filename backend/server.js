980dafd3928bd5de0ec770302b3e2d2333c4fd3e
Allan Araya Reyes <allanreyes0107@gmail.com>
2024-08-19 03:16:39 -0600

    Added posts controller and routes

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import {v2 as cloudinary} from "cloudinary"
+import {v2 as cloudinary} from "cloudinary";
 import connectMongoDB from "./db/connectMongoDB.js"
 
 
 import authRoutes from "./routes/auth.js";
-import userRoutes from "./routes/user.routes.js"
+import userRoutes from "./routes/user.routes.js";
+import postRoutes from "./routes/posts.routes.js";
 
 dotenv.config();
 
@@ -26,6 +27,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
 
 
 app.listen(PORT, () => {
